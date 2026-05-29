e6390eca742e997e85b41bce33d72f31947fe08c
TIC200-UPO <agarcon@upo.es>
2026-05-29 13:29:46 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+setup(
+    name = 'confidence_correctness_matrix',
+    version = '1.0.0',
+    author = 'Jesús S. Aguilar-Ruiz, Alejandro García Conde',
+    #author_email=,
+    description = 'Confidence-Correctness Matrix',
+    long_description = (HERE / "README.md").read_text(encoding='utf-8'), 
+    long_description_content_type = "text/markdown",
+    #url=,
+    packages=["confidence_correctness_matrix"],
+    # classifiers=,
+    # python_requires=,
+    install_requires = ["numpy","pandas","matplotlib"],
+    license = "BSD 3-Clause License"
+)
